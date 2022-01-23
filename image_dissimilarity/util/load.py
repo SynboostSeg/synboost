69c9b83356bb4e1c4ffa0bc2742fe7f355cdda03
SynboostSeg <96657684+SynboostSeg@users.noreply.github.com>
2022-01-23 14:44:58 +0530

    Update load.py

--- image_dissimilarity/util/load.py
+++ image_dissimilarity/util/load.py
@@ -10,7 +10,8 @@ def load_ckp(wandb_base_path, name, epoch):
     # load check point
     path = "results/"+name + "/" + name + "_" + str(epoch) + ".pth"
     load_path = os.path.join(wandb_base_path, path)
-    wandb.restore(path)
+    #wandb.restore(path)
+    load_path = '/kaggle/input/authors-synboost-models/models/image-dissimilarity/best_net_baseline_void_prior_spadedecoder_mult_3.pth'
     checkpoint = torch.load(load_path)
 
     return checkpoint
