17a6cb7e9650caa26e153168e5ab6a818f20f206
SynboostSeg <96657684+SynboostSeg@users.noreply.github.com>
2022-01-23 14:40:08 +0530

    Update test_ensemble.py

--- image_dissimilarity/test_ensemble.py
+++ image_dissimilarity/test_ensemble.py
@@ -29,6 +29,7 @@ parser.add_argument('--wandb_run_id', type=str, default=None, help='Previous Run
 parser.add_argument('--wandb_run', type=str, default=None, help='Name of wandb run')
 parser.add_argument('--wandb_project', type=str, default="MLRC_Synboost", help='wandb project name')
 parser.add_argument('--wandb', type=bool, default=True, help='Log to wandb')
+parser.add_argument('--epoch', type=int, default=12, help='best epoch number in wandb')
 
 opts = parser.parse_args()
 cudnn.benchmark = True
@@ -50,7 +51,7 @@ def grid_search(model_num=4):
     d = {}
     w = [0, 1, 2, 3]
     best_score, best_roc, best_ap, best_weights = 1.0, 0, 0, None
-    best = 0
+    best = -1
     # iterate all possible combinations (cartesian product)
     for weights in product(w, repeat=model_num):
         # skip if all weights are equal
@@ -167,7 +168,7 @@ if __name__ == '__main__':
     wandb_utils.init_wandb(config=config, key=opts.wandb_Api_key,wandb_project= opts.wandb_project, wandb_run=opts.wandb_run, wandb_run_id=opts.wandb_run_id, wandb_resume=opts.wandb_resume)
     diss_model.eval()
     if use_wandb and wandb_resume:
-        checkpoint = load_ckp(config["wandb_config"]["model_path_base"], "best", 12)
+        checkpoint = load_ckp(config["wandb_config"]["model_path_base"], "best", opts.epoch)
         diss_model.load_state_dict(checkpoint['state_dict'], strict=False)
     
     softmax = torch.nn.Softmax(dim=1)
